bd9eb6bd0d8bf67eab60c7f059c266c83f272edd
shagbag913 <sh4gbag913@gmail.com>
2020-04-21 12:35:24 -0400

    backlight: fix indention
    Thanks vim-sleuth
    
    Change-Id: Ice3e662f457b50cca4d7ed7c7cf6e97863d04a73

--- backlight.cpp
+++ backlight.cpp
@@ -10,60 +10,60 @@ namespace fs = std::filesystem;
 namespace backlight {
 
     std::string getBacklightPercentageString() {
-	std::string backlightSysfsPath = "/sys/class/backlight/";
-	for (const auto& entry : fs::directory_iterator(backlightSysfsPath)) {
-	    backlightSysfsPath = entry.path().string();
-	    break;
-	}
-	if (backlightSysfsPath == "/sys/class/backlight/")
-	    return "";
+        std::string backlightSysfsPath = "/sys/class/backlight/";
+        for (const auto& entry : fs::directory_iterator(backlightSysfsPath)) {
+            backlightSysfsPath = entry.path().string();
+            break;
+        }
+        if (backlightSysfsPath == "/sys/class/backlight/")
+            return "";
 
-	std::ifstream brightness;
-	brightness.open(backlightSysfsPath + "/max_brightness");
+        std::ifstream brightness;
+        brightness.open(backlightSysfsPath + "/max_brightness");
 
-	std::string buf;
-	getline(brightness, buf);
-	brightness.close();
-	int maxBrightness = std::stoi(buf);
+        std::string buf;
+        getline(brightness, buf);
+        brightness.close();
+        int maxBrightness = std::stoi(buf);
 
-	brightness.open(backlightSysfsPath + "/brightness");
-	getline(brightness, buf);
-	brightness.close();
-	int currentBrightness = std::stoi(buf);
+        brightness.open(backlightSysfsPath + "/brightness");
+        getline(brightness, buf);
+        brightness.close();
+        int currentBrightness = std::stoi(buf);
 
-	float percentage = ((float)currentBrightness / maxBrightness) * 100;
-	std::string brightnessPercentageString;
+        float percentage = ((float)currentBrightness / maxBrightness) * 100;
+        std::string brightnessPercentageString;
 
-	if (percentage == 100)
-	    brightnessPercentageString = "100";
-	else {
-	    brightnessPercentageString = std::to_string(percentage);
-	    if (percentage < 10)
-		brightnessPercentageString.resize(1);
-	    else
-		brightnessPercentageString.resize(2);
-	}
+        if (percentage == 100)
+            brightnessPercentageString = "100";
+        else {
+            brightnessPercentageString = std::to_string(percentage);
+            if (percentage < 10)
+                brightnessPercentageString.resize(1);
+            else
+                brightnessPercentageString.resize(2);
+        }
 
-	return brightnessPercentageString;
+        return brightnessPercentageString;
     }
 
     void loop() {
-	std::string oldBrightnessString, newBrightnessString;
+        std::string oldBrightnessString, newBrightnessString;
 
-	while (true) {
-	    newBrightnessString = " " + getBacklightPercentageString() + "%";
-	    if (newBrightnessString != oldBrightnessString) {
-		oldBrightnessString = newBrightnessString;
-		printBuffer(newBrightnessString, "backlight");
-	    }
+        while (true) {
+            newBrightnessString = " " + getBacklightPercentageString() + "%";
+            if (newBrightnessString != oldBrightnessString) {
+                oldBrightnessString = newBrightnessString;
+                printBuffer(newBrightnessString, "backlight");
+            }
 
-	    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-	}
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        }
     }
 
     std::thread start() {
-	std::thread t1(loop);
-	return t1;
+        std::thread t1(loop);
+        return t1;
     }
 }
 
